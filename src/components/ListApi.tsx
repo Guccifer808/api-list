72bdec932a88e2eeb706f9340c4f8e1634168f80
G808 <hucciallstars@gmail.com>
2023-03-01 22:55:47 +0200

    Added loading skeleton

--- src/components/ListApi.tsx
+++ src/components/ListApi.tsx
@@ -7,6 +7,26 @@ interface ListApiProps {
 }
 
 const ListApi: React.FC<ListApiProps> = ({ response, loading }) => {
+  // Loading skeleton
+  if (loading) {
+    return (
+      <div className='mt-2 grid md:grid-cols-3 gap-4 animate-pulse'>
+        {[...Array(5).keys()].map((number) => (
+          <div
+            className='h-28 w-full bg-gray-300 m-1 rounded-xl'
+            key={number}
+          ></div>
+        ))}
+      </div>
+    );
+  }
+  if (!response.entries) {
+    return (
+      <p className='text-center text-gray-500 text-2xl m-20'>
+        Oops! Somethings went wrong...
+      </p>
+    );
+  }
   return (
     <div className='mx-2 mb-10'>
       <h3 className='font-semibold text-xl text-gray-700 text-center m-4'>
